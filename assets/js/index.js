7af7b6eb1db7328377f337d636755450af113af7
Yuqing He <yhe47@id.iit.edu>
2020-05-05 00:53:22 -0500

    add reatart function

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,13 @@ let scoreElement = document.querySelector('#score');
 let score = 0;
 let numOfFruitSameTime = 1;
 let lifeElement = document.querySelector('#life');
-let life = 20;
+let life = 10;
 var fruitList = [];
 var fruitCat = {};
 let image;
+var restartButton = document.getElementById('restart');
 
+restartButton.style.display = 'none';
 function updateScore(addToScore) {
   score += addToScore;
 
@@ -21,6 +23,9 @@ function updateLife(loseLife) {
   }
   // console.log('life:' + life);
   lifeElement.innerText = life;
+  if (life == 0) {
+    restartButton.style.display = 'block';
+  }
 }
 
 document.addEventListener('mousemove', getMouse);
@@ -42,56 +47,6 @@ function draw() {
 loadImages();
 
 //random y
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-const apple = new fruit(30, 10, 0, 4, 'apple', './assets/img/apple.png');
-const mango = new fruit(
-  150,
-  apple.y - getRandomArbitrary(100, 500),
-  0,
-  3,
-  'mango',
-  './assets/img/mango.png'
-);
-
-const grape = new fruit(
-  250,
-  mango.y - getRandomArbitrary(100, 500),
-  0,
-  3,
-  'grape',
-  './assets/img/grape.png'
-);
-
-const orange = new fruit(
-  400,
-  grape.y - getRandomArbitrary(100, 500),
-  0,
-  3,
-  'orange',
-  './assets/img/orange.png'
-);
-
-const peach = new fruit(
-  550,
-  orange.y - getRandomArbitrary(100, 500),
-  0,
-  3,
-  'peach',
-  './assets/img/peach.png'
-);
-
-const pear = new fruit(
-  700,
-  peach.y - getRandomArbitrary(100, 500),
-  0,
-  3,
-  'pear',
-  './assets/img/pear.png'
-);
 
 const basket = new Basket(0, 330, 0, 0);
 
@@ -135,18 +90,7 @@ const updateFrame = () => {
       }
     }
   }
-  // apple.tick();
-  // mango.tick();
-  // grape.tick();
-  // orange.tick();
-  // peach.tick();
-  // pear.tick();
-  // apple.tick();
-  // mango.tickM();
-  // grape.tickG();
-  // orange.tickO();
-  // peach.tickP();
-  // pear.tickPear();
+
   basket.tickB();
 
   window.requestAnimationFrame(updateFrame);
@@ -155,106 +99,13 @@ const updateFrame = () => {
 
 updateFrame();
 
-// const apple = new Apple(40, -1, 0, 3);
-
-// const tick = () => {
-//   console.log("Tick");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   apple.tick();
-
-//   window.requestAnimationFrame(tick);
-// };
-
-// tick();
-
-// // mango
-// const mango = new Mango(110, -1, 0, 3);
-
-// const tickM = () => {
-//   console.log("TickM");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   mango.tickM();
-
-//   window.requestAnimationFrame(tickM);
-// };
-
-// tickM();
-
-// // grape
-// const grape = new Grape(180, -1, 0, 3);
-
-// const tickG = () => {
-//   console.log("TickG");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   grape.tickG();
-
-//   window.requestAnimationFrame(tickG);
-// };
-
-// tickG();
-
-// // orange
-// const orange = new Orange(250, -1, 0, 3);
-
-// const tickO = () => {
-//   console.log("TickO");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   orange.tickO();
-
-//   window.requestAnimationFrame(tickO);
-// };
-
-// tickO();
-
-// // peach
-// const peach = new Peach(320, -1, 0, 3);
-
-// const tickP = () => {
-//   console.log("TickP");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   peach.tickP();
-
-//   window.requestAnimationFrame(tickP);
-// };
-
-// tickP();
-
-// // pear
-// const pear = new Pear(400, -1, 0, 3);
-
-// const tickPear = () => {
-//   console.log("TickPear");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   pear.tickPear();
-
-//   window.requestAnimationFrame(tickPear);
-// };
-
-// tickPear();
-
-// // basket
-// const basket = new Basket(10, 300, 3, 0);
-
-// const tickB = () => {
-//   console.log("TickB");
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   basket.tickB();
-
-//   window.requestAnimationFrame(tickB);
-// };
-
-// tickB();
-
-// const apple = new Apple(30, -10, 0, 3);
-// const mango = new Mango(150, -10 - getRandomArbitrary(200, 1000), 0, 3);
-// const grape = new Grape(250, -10 - getRandomArbitrary(220, 1000), 0, 3);
-// const orange = new Orange(400, -10 - getRandomArbitrary(200, 1000), 0, 3);
-// const peach = new Peach(550, -10 - getRandomArbitrary(200, 1000), 0, 3);
-// const pear = new Pear(700, -10 - getRandomArbitrary(200, 1000), 0, 3);
+function restartGame() {
+  life = 10;
+  score = 0;
+  scoreElement.innerText = score;
+  lifeElement.innerText = life;
+  restartButton.style.display = 'none';
+  // var click = document.getElementById('restart');
+  // restartButton.style.display = 'none';
+  // document.location.reload();
+}
